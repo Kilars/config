56ba9446bd7aa6247c5e74291599cfb886a880a1
Lars Skifjeld <larssski@proton.me>
2025-11-16 12:00:49 +0100

    add git config and bash aliases

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -6,6 +6,25 @@
   home.username = "larsski";
   home.homeDirectory = "/home/larsski";
 
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  fonts.fontconfig.enable = true;
+  home.packages = with pkgs; [
+    nerd-fonts.fira-code
+  ];
+
+  programs.kitty = {
+    enable = true;
+    font.size = 15;
+    font.name = "FiraCode Nerd Font";
+    themeFile = "Argonaut";
+    settings = {
+      disable_ligatures = "never";
+      copy_on_select = "yes";
+      background_opacity = 0.87;
+    };
+  };
+
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
@@ -15,26 +34,6 @@
   # release notes.
   home.stateVersion = "25.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
@@ -73,4 +72,30 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+  programs.git = {
+    enable = true;
+    settings = {
+      user.name = "Lars Skifjeld";
+      user.email = "larssski@proton.me";
+      init.defaultBranch = "main";
+      core.editor = "nvim";
+      pull.rebase = true;
+      rerere.enabled = true;
+    };
+  };
+
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      ".." = "cd ..";
+      "..." = "cd ../..";
+      "...." = "cd ../../..";
+      la = "ls -a";
+      g = "git status";
+      gcan = "git commit -a --amend --no-edit";
+      vim = "nvim";
+      clc = "fc -ln -1 | sed 's/^\s*//' | sed 's/\s*$//' | tr -d '\n' | xsel --clipboard";
+      lt = "exa -aT";
+    };
+  };
 }
